c85ab0136412c14460b3ab61114504f629e9e2f3
razzor-codes <incredibledazzler@gmail.com>
2022-07-30 23:39:46 +0530

    dark mode stateful

--- css/toggle.js
+++ css/toggle.js
@@ -1,10 +1,36 @@
+let storedColor = sessionStorage.getItem('toggledColor');
+
+let theme = sessionStorage.getItem('theme');
+if (theme === 'dark'){
+    document.body.classList.toggle('dark');
+}
+if (storedColor != null && storedColor === 'white' ){
+    try{
+        document.getElementById('title').style.color = storedColor;
+        document.getElementById('contract-title').style.color = storedColor;
+        document.getElementById('diff-title').style.color = storedColor;
+    }
+    catch(err){}
+
+}
 checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', ()=>{
-document.body.classList.toggle('dark');
 var color = document.getElementById('title').style.color;
+
+document.body.classList.toggle('dark');
+
 var toggledColor = color==='white'?'black':'white';
-document.getElementById('title').style.color = toggledColor;
-document.getElementById('contract-title').style.color = toggledColor;
-document.getElementById('diff-title').style.color = toggledColor;
+
+sessionStorage.setItem('toggledColor', toggledColor);
+sessionStorage.setItem('theme', document.body.classList.value);
+try{
+    document.getElementById('title').style.color = toggledColor;
+    document.getElementById('contract-title').style.color = toggledColor;
+    document.getElementById('diff-title').style.color = toggledColor;
+}
+catch(err) {
+
+}
+
 });
