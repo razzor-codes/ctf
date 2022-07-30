7dacf841ac6be0708bae2c384276811c800a508c
razzor-codes <incredibledazzler@gmail.com>
2022-07-30 15:25:23 +0530

    dark mode

---
+++ css/toggle.js
@@ -0,0 +1,9 @@
+checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+document.body.classList.toggle('dark');
+var color = document.getElementById('title').style.color;
+var toggledColor = color==='white'?'black':'white';
+document.getElementById('title').style.color = toggledColor;
+document.getElementById('contract-title').style.color = toggledColor;
+});
